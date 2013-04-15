eca6a15d0266bb08703b61274ecf6d8a99cb4d4b
Erik Alsmyr <erik@alsmyr.se>
2013-04-15 14:30:22 +0200

    Working client side

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var express = require('express')
-  , app = express.createServer();
+var express = require('express');
+var app = express();
 
-app.get('/', function(req, res) {
-  res.send('hello world');
+app.get(/^(.+)$/, function(req, res) 
+{ 
+console.log(req.params[0] + ' requested');
+res.sendfile('client/' + req.params[0]); 
+
 });
+
+console.log('Listening on port 3000');
 
 app.listen(3000)
